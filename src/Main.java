eaeac1cbee22aba2d749756353053a498c2764b6
HelpBringer <dariankaran@mail.ru>
2022-08-24 14:28:30 +0300

    Commit experience

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println(String1.helloName("World"));
         System.out.println(String1.left2("Hi"));
+        System.out.println();
     }
 
 }
