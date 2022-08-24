abd6a7d62c001db59a2c4517a6acc2108cccf384
HelpBringer <dariankaran@mail.ru>
2022-08-24 16:01:51 +0300

    Final logic commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 import stringTasks.*;
 import arrayTasks.*;
-
+import logicTasks.*;
 
 
 public class Main {
@@ -14,6 +14,9 @@ public class Main {
 
         Array2 arr2 = new Array2();
         System.out.println(arr2.has22(new int[]{1, 2, 2}));
+
+        Logic2 logic2 = new Logic2();
+        System.out.println(logic2.makeChocolate(4, 1, 9));
     }
 
 }
